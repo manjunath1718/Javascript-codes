60432cc12c5764205ea9de1a060ac6c99ce4b46a
manjunath <betagerimanju7@gmail.com>
2025-01-16 19:56:54 +0530

    2nd commit

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"//traets all js code as newwr version
+
+//alert(3+3)//we are using node not browser
+
+console.log(3+3);
+console.log("rajesh");
+
+let name="suresh"
+let age=23
+let isLoggedIn=true
+let state;
+
+/* 
+
+number => 2 to power 53
+BigInt  
+String => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+Object
+
+*/
+
+console.log(typeof age);
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "suresh";
+
+// console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+/* 
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+let isLoggedIn = "suresh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+/* 
+1 => true; 0 => false
+"" =>false
+" " => true
+"suresh" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// **********************Operations*************************
