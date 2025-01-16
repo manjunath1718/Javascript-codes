93709f8b2c9c3b3cb92f023ca5e9f16017661994
manjunath <betagerimanju7@gmail.com>
2025-01-16 22:59:47 +0530

    3rd commit

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -31,7 +31,44 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // **********************Operations*************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);//-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);//6
+// console.log(2**3);//8
+// console.log(2/2);//1
+// console.log(2 / 3); //0.6666666666666666
+// console.log(4%2);//0
+// console.log(4 % 5); //4
+
+let str1 = "hello!";
+let str2 = " Ganesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 4;
+// console.log(num2);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
